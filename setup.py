bb1922b0df35a9e8677355d8356452ecfd0ea452
Benoit Suttor <bsuttor@imio.be>
2019-02-12 18:11:28 +0100

    Remove not used dependency

--- setup.py
+++ setup.py
@@ -19,15 +19,13 @@ requires = [
     'pyramid_zodbconn',
     'transaction',
     'ZODB3',
-    'cornice',
-    'cornice_swagger',
 ]
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest >= 3.7.4',
     'pytest-cov',
-    'requests',
+    'ipdb',
 ]
 
 setup(

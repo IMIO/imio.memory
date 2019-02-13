bef78c85a766214c8fb7092ea859c2d496e25698
Benoit Suttor <bsuttor@imio.be>
2019-02-13 09:47:33 +0100

    Update version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='memory',
-    version='0.0',
+    version='0.1',
     description='memory',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
